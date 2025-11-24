7180d6172e938ac422fdf39999fc65e255e38fd9
Awais-404 <155368467+Awais-404@users.noreply.github.com>
2025-11-25 01:03:07 +0500

    added books, added footer

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const book_array = Array.from(document.querySelectorAll('.book-array'));
-window.onload=set_scroll_btn_visibility();
+const book_arrays = Array.from(document.querySelectorAll('.book-array'));
+const book_template = document.getElementById("book-template")
+
+window.onload = function(){
+  fill_book_arrays()
+  setTimeout(() => {
+    set_scroll_btn_visibility()
+  }, 100);
+}
 window.addEventListener("resize", set_scroll_btn_visibility);
+
+function fill_book_arrays(){
+  book_arrays.forEach(book_array => {
+    for (let i = 0; i < 10; i++) {
+      let book = book_template.content.cloneNode(true);
+      book.querySelector(".book-cover").setAttribute('src',"Books/"+(book_array.parentElement.querySelector("h1").textContent)+"/"+(i+1)+".jpg")
+      book_array.appendChild(book);
+    }
+  });
+}
 
 function set_scroll_btn_visibility(){
   let over= false;
-  if (book_array[0].scrollWidth > book_array[0].clientWidth) {
-    over = true;
-  }
-  else{
-    over = false;
-  }
-  if (over) {
-    book_array.forEach(element => {
+  book_arrays.forEach(element => {
+    if (element.scrollWidth > element.clientWidth) {
+      over = true;
+    }
+    else{
+      over = false;
+    }
+    if (over) {
       element.querySelector('.scroll-left-button').style.visibility = 'visible';
       element.querySelector('.scroll-right-button').style.visibility = 'visible';
       if (element.scrollLeft == '0') {
@@ -20,15 +37,13 @@ function set_scroll_btn_visibility(){
       var scroll_max = element.scrollWidth - element.clientWidth;
       if (element.scrollLeft/scroll_max >= 1) {
         element.querySelector('.scroll-right-button').style.visibility = 'hidden';
-      }
-    });
-  }
-  else{
-    book_array.forEach(element => {
-      element.querySelector('.scroll-left-button').style.visibility = 'hidden';
-      element.querySelector('.scroll-right-button').style.visibility = 'hidden';
-    });
-  }
+      };
+    }
+    else{
+        element.querySelector('.scroll-left-button').style.visibility = 'hidden';
+        element.querySelector('.scroll-right-button').style.visibility = 'hidden';
+    }
+  })
 }
 
 function scroll_left(elem) {
@@ -44,8 +59,8 @@ function scroll_right(elem) {
   let scroll_elem = elem.parentElement;
   scroll_elem.scrollLeft +=300;
   scroll_elem.querySelector('.scroll-left-button').style.visibility = 'visible';
-  var scroll_max = scroll_elem.scrollWidth - scroll_elem.clientWidth;
-  if (scroll_elem.scrollLeft/scroll_max >= 1) {
+  var scroll_max = scroll_elem.scrollWidth -Math.max(scroll_elem.clientWidth) ;
+  if (scroll_elem.scrollLeft/scroll_max >= 0.99) {
     elem.style.visibility = 'hidden';
   }
 }
